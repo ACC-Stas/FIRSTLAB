aefcb15a6dcd184313a2f695f488cc7494e2a3a4
XoTaBuCh <tanzanit2003@gmail.com>
2022-03-20 09:11:12 -0700

    client reg

--- src/main/java/main/banksystem/controllers/RegistrationMenuController.java
+++ src/main/java/main/banksystem/controllers/RegistrationMenuController.java
@@ -1,9 +1,14 @@
 package main.banksystem.controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class RegistrationMenuController {
 
@@ -30,7 +35,22 @@ public class RegistrationMenuController {
 
     @FXML
     void initialize() {
+        clientSignUpButton.setOnAction(event -> {
+            clientSignUpButton.getScene().getWindow().hide();
 
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/main/banksystem/client_registration_menu.fxml"));
+
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        });
 
     }
 
