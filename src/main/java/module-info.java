53378d477b5bf1390eb8768cacd794ddd607882b
Станислав Кононович <ksv.2002@mail.ru>
2022-03-20 15:05:53 +0300

    add opencsv dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,7 @@ module main.banksystem {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires com.opencsv;
 
     opens main.banksystem to javafx.fxml;
     exports main.banksystem;
