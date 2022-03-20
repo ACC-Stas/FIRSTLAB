e95204a621de3793eb994b285daa79ffbf56ca2e
Станислав Кононович <ksv.2002@mail.ru>
2022-03-20 18:33:37 +0300

    add CPU

--- src/main/java/main/banksystem/CPU.java
+++ src/main/java/main/banksystem/CPU.java
@@ -1,4 +1,42 @@
 package main.banksystem;
 
+import java.util.Queue;
+import java.util.Stack;
+
 public class CPU {
+    CPU(User cpuUser) {
+        dataBase = DataBase.GetInstance();
+        queueConverter = new StringConverter<>();
+        stackConverter = new StringConverter<>();
+        this.cpuUser = cpuUser;
+    }
+
+    public User getCpuUser() {
+        return cpuUser;
+    }
+
+    public void setCpuUser(User cpuUser) {
+        this.cpuUser = cpuUser;
+    }
+
+    private User cpuUser;
+    private final DataBase dataBase;
+    private final StringConverter<Queue<ICommand>> queueConverter;
+    private final StringConverter<Stack<ICommand>> stackConverter;
+
+    public void HeldCommand(ICommand command) {
+        if (command.GetType().isApprovable()) {
+            Queue<ICommand> queue = queueConverter.Deserialize(dataBase.Download(cpuUser.getIdx(), "queue.csv"));
+            queue.add(command);
+            dataBase.Save(cpuUser.getIdx(), "queue.csv", queueConverter.Serialize(queue));
+            return;
+        }
+
+        command.execute();
+        if (command.GetType().isSaveable()) {
+            Stack<ICommand> stack = stackConverter.Deserialize(dataBase.Download(cpuUser.getIdx(), "stack.csv"));
+            stack.add(command);
+            dataBase.Save(cpuUser.getIdx(), "stack.csv", stackConverter.Serialize(stack));
+        }
+    }
 }
--- src/main/java/main/banksystem/User.java
+++ src/main/java/main/banksystem/User.java
@@ -1,12 +1,60 @@
 package main.banksystem;
 
 public class User {
-    public User(Passport passport, Id idx, String number, String email, String password, Role role){
+    public User(Passport passport, Id idx, String number, String email, String password, Role role) {
         this.passport = passport;
         this.idx = idx;
         this.number = number;
         this.email = email;
         this.password = password;
+        this.role = role;
+    }
+
+    public Passport getPassport() {
+        return passport;
+    }
+
+    public void setPassport(Passport passport) {
+        this.passport = passport;
+    }
+
+    public Id getIdx() {
+        return idx;
+    }
+
+    public void setIdx(Id idx) {
+        this.idx = idx;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
         this.role = role;
     }
 
