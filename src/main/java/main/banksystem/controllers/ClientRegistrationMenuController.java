4b7f911db614f70a0c2e09cd097c0830e26134ee
XoTaBuCh <tanzanit2003@gmail.com>
2022-03-20 10:07:00 -0700

    client reg2

--- src/main/java/main/banksystem/controllers/ClientRegistrationMenuController.java
+++ src/main/java/main/banksystem/controllers/ClientRegistrationMenuController.java
@@ -1,12 +1,10 @@
 package main.banksystem.controllers;
 
 import java.net.URL;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.collections.ObservableList;
 import javafx.collections.FXCollections;
 import main.banksystem.Citizenship;
@@ -41,6 +39,9 @@ public class ClientRegistrationMenuController {
     private TextField emailField;
 
     @FXML
+    private Label errorLabel;
+
+    @FXML
     private TextField fatherName;
 
     @FXML
@@ -70,6 +71,21 @@ public class ClientRegistrationMenuController {
         sexStatus.setValue(sexList.get(0));
         citizenshipStatus.setItems(citizenshipList);
         citizenshipStatus.setValue(citizenshipList.get(0));
+        registrationButton.setOnAction(event -> {
+            String name = nameField.getText();
+            String surname = surnameField.getText();
+            String fathername = fatherName.getText();
+            String email = emailField.getText();
+            String number = numberField.getText();
+            String date = dateOfBirthday.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            String sex = sexStatus.getValue();
+            String citizenship = citizenshipStatus.getValue();
+            String idPassport = idField.getText();
+            String country = countryField.getText();
+            String city = cityField.getText();
+            String streetAddress = streetField.getText();
+
+        });
     }
 
 }
