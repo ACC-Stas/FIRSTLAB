cd7f276de3a3a51f4f31bc518adb564864137ed0
Станислав Кононович <ksv.2002@mail.ru>
2022-03-22 16:21:00 +0300

    some things

--- src/main/java/main/banksystem/builders/CompanyBuilder.java
+++ src/main/java/main/banksystem/builders/CompanyBuilder.java
@@ -1,6 +1,8 @@
 package main.banksystem.builders;
 
+import main.banksystem.containers.BIC;
 import main.banksystem.containers.Company;
+import main.banksystem.containers.Id;
 import main.banksystem.containers.Role;
 
 public class CompanyBuilder {
@@ -23,13 +25,46 @@ public class CompanyBuilder {
         company.setType(type);
     }
 
-    public void BuildType(String name) {
+    public void BuildType(String type) {
         try {
-            company.setType(Company.Type.valueOf(name));
+            company.setType(Company.Type.valueOf(type));
         } catch (IllegalArgumentException ignored) {
         }
     }
 
+    public void BuildName(String name) {
+        company.setjName(name);
+    }
+
+    public void BuildBunkId(BIC bic) {
+        company.setBankID(bic);
+    }
+
+    public void BuildBunkId(String bic) {
+        Id idx;
+        try {
+            idx = new Id(Long.parseLong(bic));
+        } catch (IllegalArgumentException e) {
+            return;
+        }
+
+        company.setBankID(new BIC(idx));
+    }
+
+    public void BuildPAN(Id pan) {
+        company.setPAN(pan);
+    }
+
+    public void BuildPAN(String pan) {
+        Id idx;
+        try {
+            idx = new Id(Long.parseLong(pan));
+        } catch (IllegalArgumentException e) {
+            return;
+        }
+
+        company.setPAN(idx);
+    }
 
 
     public Result getCompany() {
