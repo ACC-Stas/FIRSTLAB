e1f9672b485075b5f016e4466123808cd8afafcf
XoTaBuCh <tanzanit2003@gmail.com>
2022-03-23 09:51:24 -0700

    huita

---
+++ src/main/java/main/banksystem/commands/BuildBillCommand.java
@@ -0,0 +1,4 @@
+package main.banksystem.commands;
+
+public class BuildBillCommand {
+}
--- src/main/java/main/banksystem/controllers/ManagerMainMenuController.java
+++ src/main/java/main/banksystem/controllers/ManagerMainMenuController.java
@@ -1,4 +1,64 @@
 package main.banksystem.controllers;
 
+import java.net.URL;
+import java.util.Map;
+import java.util.Queue;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import main.banksystem.DataBase;
+import main.banksystem.StringConverter;
+import main.banksystem.commands.ICommand;
+import main.banksystem.containers.Id;
+
 public class ManagerMainMenuController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Accordion creditsAndInstallmentAccordion;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private Button findButton;
+
+    @FXML
+    private Accordion historyAccordion;
+
+    @FXML
+    private TextField idSpecialistField;
+
+    @FXML
+    private Accordion registrationAccordion;
+
+    @FXML
+    private Button toClientButton;
+
+    @FXML
+    private Button toManagerButton;
+
+    @FXML
+    void initialize() {
+        for (int i = 0; i < 2; i++) {
+            TitledPane titledPane = new TitledPane();
+            titledPane.setText("Клиент " + i.toString());
+
+            VBox content1 = new VBox();
+            content1.getChildren().add(new Label("Java Swing Tutorial"));
+            content1.getChildren().add(new Label("JavaFx Tutorial"));
+            content1.getChildren().add(new Label("Java IO Tutorial"));
+
+            titledPane.setContent(content1);
+            registrationAccordion.getPanes().addAll(titledPane);
+        }
+    }
+
 }
