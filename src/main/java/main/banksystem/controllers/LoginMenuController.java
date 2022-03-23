0ebd605ac346e6f83c0355a08e3e714e735e7b43
XoTaBuCh <tanzanit2003@gmail.com>
2022-03-23 13:34:34 -0700

    Finish login

--- src/main/java/main/banksystem/controllers/LoginMenuController.java
+++ src/main/java/main/banksystem/controllers/LoginMenuController.java
@@ -1,11 +1,17 @@
 package main.banksystem.controllers;
 
 import java.net.URL;
+import java.util.Map;
+import java.util.Objects;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import main.banksystem.DataBase;
+import main.banksystem.containers.Id;
+import main.banksystem.containers.User;
 
 import static main.banksystem.controllers.SwitchMenu.switchMenu;
 
@@ -16,6 +22,9 @@ public class LoginMenuController {
 
     @FXML
     private URL location;
+
+    @FXML
+    private Label errorLabel;
 
     @FXML
     private TextField loginField;
@@ -41,9 +50,32 @@ public class LoginMenuController {
             switchMenu(loginSignUpButton, "/main/banksystem/company_registration_menu.fxml");
         });
         loginSignInButton.setOnAction(actionEvent -> {
-            String login = loginField.getText();
-            String password = passwordField.getText();
 
+            DataBase dataBase = DataBase.GetInstance();
+            Map<Id, User> users = dataBase.DownloadMap(DataBase.USER_PART, User.class);
+            for (User user : users.values()) {
+                if (Objects.equals(user.getLogin(), loginField.getText()) && Objects.equals(user.getPassword(), passwordField.getText())) {
+                    switch (user.getRole()) {
+                        case CLIENT -> {
+                            switchMenu(loginSignUpButton, "/main/banksystem/client_main_menu.fxml");
+                        }
+                        case OPERATOR -> {
+                            switchMenu(loginSignUpButton, "/main/banksystem/operator_main_menu.fxml");
+                        }
+                        case MANAGER -> {
+                            switchMenu(loginSignUpButton, "/main/banksystem/manager_main_menu.fxml");
+                        }
+                        case SPECIALIST -> {
+                            switchMenu(loginSignUpButton, "/main/banksystem/specialist_main_menu.fxml");
+                        }
+                        case ADMINISTRATOR -> {
+                            switchMenu(loginSignUpButton, "/main/banksystem/administrator_main_menu.fxml");
+                        }
+                    }
+                }
+            }
+
+            errorLabel.setText("Wrong");
         });
 
     }
