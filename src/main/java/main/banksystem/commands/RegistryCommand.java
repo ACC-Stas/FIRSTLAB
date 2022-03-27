b6b754dc46600364fa24d768d00abf39999be78e
Станислав Кононович <ksv.2002@mail.ru>
2022-03-28 01:17:11 +0300

    create set bill status command

--- src/main/java/main/banksystem/commands/RegistryCommand.java
+++ src/main/java/main/banksystem/commands/RegistryCommand.java
@@ -19,6 +19,20 @@ public class RegistryCommand implements ICommand {
     private User user;
     private String description;
 
+    public RegistryCommand(User user, ICommand.Type type) {
+        this.user = user;
+        this.type = type;
+        this.description = String.format("User %s wants to register in system. With passport id %d.",
+                user.getPassport().getFullName().getFirstName(), user.getPassport().getIdx().getId());
+    }
+
+    @JsonCreator
+    public RegistryCommand() {
+        this.user = null;
+        this.type = null;
+        this.description = "No description";
+    }
+
     @Override
     public Type getType() {
         return type;
@@ -40,20 +54,6 @@ public class RegistryCommand implements ICommand {
     @Override
     public void setDescription(String description) {
         this.description = description;
-    }
-
-    public RegistryCommand(User user, ICommand.Type type) {
-        this.user = user;
-        this.type = type;
-        this.description = String.format("User %s wants to register in system. With passport id %d.",
-                user.getPassport().getFullName().getFirstName(), user.getPassport().getIdx().getId());
-    }
-
-    @JsonCreator
-    public RegistryCommand() {
-        this.user = null;
-        this.type = null;
-        this.description = "No description";
     }
 
     @Override
