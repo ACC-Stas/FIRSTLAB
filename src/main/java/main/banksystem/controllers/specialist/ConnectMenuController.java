615516f39a3b89910209296ea75ee5513fd6ad85
Станислав Кононович <ksv.2002@mail.ru>
2022-04-02 21:27:45 +0300

    add connect button

--- src/main/java/main/banksystem/controllers/specialist/ConnectMenuController.java
+++ src/main/java/main/banksystem/controllers/specialist/ConnectMenuController.java
@@ -1,7 +1,9 @@
 package main.banksystem.controllers.specialist;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
@@ -11,6 +13,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import main.banksystem.DataBase;
+import main.banksystem.ProgramStatus;
 import main.banksystem.entities.Company;
 import main.banksystem.entities.Id;
 
@@ -36,14 +39,43 @@ public class ConnectMenuController {
     @FXML
     void initialize() {
         DataBase dataBase = DataBase.getInstance();
+        ProgramStatus status = ProgramStatus.getInstance();
         Map<Id, Company> companies = dataBase.downloadMap(DataBase.COMPANY_PART, Company.class);
         for (Company company : companies.values()) {
-            companyList.add(company.getPAN().toString());
+            companyList.add(company.getjName());
         }
         companyChoice.setItems(companyList);
 
         connectButton.setOnAction(event -> {
+            String jName = companyChoice.getValue();
+            if (jName == null || jName.equals("")) {
+                errorLabel.setText("Choose company");
+                return;
+            }
 
+            Company company = null;
+            for (Company companyI : companies.values()) {
+                if (Objects.equals(companyI.getjName(), jName)) {
+                    company = companyI;
+                    break;
+                }
+            }
+            if (company == null) {
+                errorLabel.setText("No such company");
+                return;
+            }
+
+            for (Id id : company.getSpecialistIds()) {
+                if (id.equals(status.getUser().getIdx())) {
+                    errorLabel.setText("You are already in this company");
+                    return;
+                }
+            }
+
+            company.getSpecialistIds().add(status.getUser().getIdx());
+            dataBase.save(company.getPAN(), DataBase.COMPANY_PART, company);
+
+            connectButton.getScene().getWindow().hide();
         });
     }
 
--- src/main/java/main/banksystem/controllers/specialist/SpecialistMainMenuController.java
+++ src/main/java/main/banksystem/controllers/specialist/SpecialistMainMenuController.java
@@ -1,4 +1,56 @@
 package main.banksystem.controllers.specialist;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Accordion;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import main.banksystem.controllers.SwitchMenu;
+
 public class SpecialistMainMenuController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label companyMoneyLabel;
+
+    @FXML
+    private Button createCompanyTransferButton;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private Button findButton1;
+
+    @FXML
+    private Accordion newStaffAccordion;
+
+    @FXML
+    private Accordion salaryAccordion;
+
+    @FXML
+    private Button setCompanyButton;
+
+    @FXML
+    private Label specialistStatusLabel;
+
+    @FXML
+    private Button toClientButton;
+
+    @FXML
+    private Accordion transferAccordion;
+
+    @FXML
+    void initialize() {
+        setCompanyButton.setOnAction(event -> {
+            SwitchMenu.newMenu(setCompanyButton, "/main/banksystem/specialist/connect_menu.fxml");
+        });
+    }
+
 }
